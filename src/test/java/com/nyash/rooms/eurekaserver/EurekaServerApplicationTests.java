eb0c6e58877df4359a0af4987e9193d4213be4d0
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-25 03:38:21 +0700

    Initial commit

---
+++ src/test/java/com/nyash/rooms/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyash.rooms.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
